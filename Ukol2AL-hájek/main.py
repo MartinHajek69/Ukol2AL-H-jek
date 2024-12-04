1f3e67d8209645f4d1dbc57a24ddce1e872a07dc
MartinHajek69 <159123515+MartinHajek69@users.noreply.github.com>
2024-12-04 09:18:33 +0000

    cc

--- Ukol2AL-hájek/main.py
+++ Ukol2AL-hájek/main.py
@@ -1,3 +1,4 @@
+import random
 pole = [48, 95, 21, 3, 35, 66, 69, 41, 33, 99]
 
 def bubble_sort():
@@ -10,3 +11,22 @@ def bubble_sort():
     return pole
 
 print(bubble_sort())
+
+arrayy = [-15, 54, 23, -69, 13, -89, 74, 7, 8, 15]
+
+def is_sorted(arrayy):
+    for i in range(1, len(arrayy)):
+        if arrayy[i] < arrayy[i-1]:
+            return False
+    return True
+
+def bogosort(arrayy):
+    count = 0
+    while not is_sorted(arrayy):
+        random.shuffle(arrayy)
+        count += 1
+        print(f"Pokus {count}: {arrayy}")
+    print(f"Seznam seřazen po {count} pokusech.")
+    return arrayy
+
+print("Bogo sort:", bogosort(arrayy))
